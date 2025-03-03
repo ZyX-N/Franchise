8ff6e8d8d0d5ee722b727b0f53ac31548c7203c2
Aman Verma <amanv5051@gmail.com>
2025-03-03 14:02:46 +0530

    Searchable Dropdown

--- src/app/_components/Input/Dropdown.tsx
+++ src/app/_components/Input/Dropdown.tsx
@@ -1,4 +1,4 @@
-import { ChevronDownIcon } from "@heroicons/react/24/outline";
+import { CheckIcon, ChevronDownIcon } from "@heroicons/react/24/outline";
 import { ChangeEvent, useState } from "react";
 
 interface DropdowProp {
@@ -18,7 +18,8 @@ export default function Dropdown({
   classes,
   option,
 }: DropdowProp) {
-  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [dropdownOpen, setDropdownOpen] = useState<boolean>(false);
+  const [inputSearch, setInputSearch] = useState<string>("");
 
   const focusHandler = () => {
     setDropdownOpen(true);
@@ -30,13 +31,25 @@ export default function Dropdown({
     }, 200);
   };
 
+  const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    let currentValue = e.target.value;
+    setInputSearch(currentValue);
+  };
+
+  const getInputValue: () => string = () => {
+    if (dropdownOpen) {
+      return inputSearch;
+    }
+    return value;
+  };
+
   return (
     <div className="relative">
       <input
         type="text"
         placeholder={placeholder || ""}
-        value={value}
-        onChange={() => setValue(value)}
+        value={getInputValue()}
+        onChange={onChangeHandler}
         className={`${classes} bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-[#bf9877] focus:border-[#bf9877] outline-none block w-full p-2.5 cursor-pointer`}
         id={id}
         onFocus={focusHandler}
@@ -44,22 +57,32 @@ export default function Dropdown({
       />
       {dropdownOpen && (
         <ul className="absolute top-full left-0 flex flex-col w-full rounded-md bg-white border-2 shadow-lg shadow-gray-300 max-h-[200px] overflow-x-hidden overflow-y-auto z-40">
-          {option.map((item, idx) => (
-            <li
-              key={item.value}
-              className={`cursor-pointer min-h-10 flex items-center px-4 hover:bg-[rgba(0,0,0,0.1)] ${
-                idx !== 0 ? "border-t" : ""
-              }`}
-              onClick={() => {
-                setValue(item.value);
-                setTimeout(() => {
-                  setDropdownOpen(false);
-                }, 200);
-              }}
-            >
-              {item.label}
-            </li>
-          ))}
+          {option.map(
+            (item, idx) =>
+              item.value.includes(inputSearch || "") && (
+                <li
+                  key={item.value}
+                  className={`relative cursor-pointer min-h-10 flex items-center px-4 hover:bg-[rgba(0,0,0,0.1)] ${
+                    idx !== 0 ? "border-t" : ""
+                  } ${item.value === value ? "bg-blue-200" : "bg-white"}`}
+                  onClick={() => {
+                    setValue(item.value);
+                    setInputSearch(item.value);
+                    setTimeout(() => {
+                      setDropdownOpen(false);
+                    }, 200);
+                  }}
+                >
+                  {item.label}
+                  {item.value === value && (
+                    <CheckIcon
+                      className="size-4 text-blue-500 absolute right-4 top-1/2 -translate-y-1/2"
+                      strokeWidth={3}
+                    />
+                  )}
+                </li>
+              )
+          )}
         </ul>
       )}
 
